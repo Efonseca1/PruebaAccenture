320d381ee05b8db3d34de0cae37ca9a98907850c
efonseca1 <estefaniafonseca951@gmail.com>
2018-09-22 18:47:08 -0500

    carpeta prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCrRHZ0RhYAOQHswf8381VYzaUbPIJJNl0",
+    authDomain: "pruebaaccenture-8b759.firebaseapp.com",
+    databaseURL: "https://pruebaaccenture-8b759.firebaseio.com",
+    projectId: "pruebaaccenture-8b759",
+    storageBucket: "pruebaaccenture-8b759.appspot.com",
+    messagingSenderId: "897229085344"
+  }
 };
